c16ddaee55deba2612b9d90db4036db5b509d2a1
ZEROssk <akinori_17N1101034@nnn.ed.jp>
2019-08-24 16:29:40 +0900

    Electron Hello World

---
+++ main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron')
+
+let win
+
+function createWindow() {
+
+    //ウインドウの作成
+    win = new BrowserWindow({ width:  400, height: 400 })
+
+    //ウインドウに表示する内容
+    win.loadFile('index.html')
+
+    //デバッグ画面表示
+    // win.webContents.openDevTools()
+
+    //このウインドウが閉じられたときの処理
+    win.on('closed', () => {
+	        win = null
+	    })
+}
+
+//アプリが初期化されたとき（起動されたとき）
+app.on('ready',()=>{
+    createWindow();
+})
+
+//全ウインドウが閉じられたとき
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+	        app.quit()
+	    }
+})
+
+//アクティブになったとき（MacだとDockがクリックされたとき）
+app.on('activate', () => {
+    if (win === null) {
+	        createWindow()
+	    }
+})
+
