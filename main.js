22e6a906e860aa68346c4363363581eb39c4fbb4
ZEROssk <akinori_17N1101034@nnn.ed.jp>
2019-09-01 17:49:31 +0900

    update main.js

--- main.js
+++ main.js
@@ -3,15 +3,9 @@ const { app, BrowserWindow } = require('electron')
 let win
 
 function createWindow() {
-	//var Screen = require('screen');
-	//var size = Screen.getPrimaryDisplay().size;
 	win = new BrowserWindow ({
 		left: 0,
 		top: 0,
-		//width: size.width,
-		width: 500,
-		//height: size.height,
-		height: 500,
 		frame: false,
 		show: true,
 		transparent: true,
@@ -22,10 +16,7 @@ function createWindow() {
 	win.maximize();
 
 	win.loadURL(`file://${process.cwd()}/index.html`);
-
-	//デバッグ画面表示
 	// win.webContents.openDevTools()
-
 	win.on('closed', () => {
 		win = null
 	})
