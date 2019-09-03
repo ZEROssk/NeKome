2a1ab2bd79e6e330bd5112cfe3542b10b77959ca
ZEROssk <akinori_17N1101034@nnn.ed.jp>
2019-09-03 15:48:26 +0900

    websocket test

---
+++ websocket_test/main.js
@@ -0,0 +1,60 @@
+"use strict";
+var sv = require('express')();
+var http = require('http').Server(sv);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+const { app, BrowserWindow } = require('electron')
+let win
+
+sv.get('/', function(req, res){
+  res.sendFile(__dirname + '/client.html');
+})
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+      io.emit('chat message', msg);
+    });
+})
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+})
+
+function createWindow() {
+	win = new BrowserWindow ({
+		left: 0,
+		top: 0,
+		frame: false,
+		show: true,
+		transparent: true,
+		alwaysOnTop: true
+	})
+
+	// win.setIgnoreMouseEvents(true);
+	win.maximize();
+
+	win.loadURL(`http://localhost:3000/`)
+	//win.loadURL(`file://${__dirname}/host.html`)
+	//win.webContents.openDevTools()
+	win.on('closed', () => {
+		win = null
+	})
+}
+
+app.on('ready',()=>{
+	createWindow();
+})
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit()
+	}
+})
+
+app.on('activate', () => {
+	if (win === null) {
+		createWindow()
+	}
+})
+
